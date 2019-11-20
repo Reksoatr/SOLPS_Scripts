0920da2bff811fb300e29dfa3525321ebbeae08b
Richard Reksoatmodjo <rmreksoatmodjo@email.wm.edu>
2019-11-20 09:42:13 -0500

    Update to EfoldSlider

--- SOLPS_Scripts/EfoldSliderGUI.py
+++ SOLPS_Scripts/EfoldSliderGUI.py
@@ -19,42 +19,41 @@ JXI = NeuDen.KW['JXI']
 RadLoc = NeuDen.RadCoords['RadLoc']
 VertLoc = NeuDen.RadCoords['VertLoc']
 
+f0 = JXA
+axcolor = 'lightgoldenrodyellow'
 
 fig, ax = plt.subplots()
 plt.subplots_adjust(left=0.25, bottom=0.25)
 
-NeuDen.Contour('NeuDen',LOG10=2,AX=ax, Markers=False)
-ax.set_title('Neutral Density')
+axcontour = fig.add_subplot(122)
+NeuDen.Contour('NeuDen',LOG10=1,AX=axcontour, Markers=False)
+axcontour.set_title('Neutral Density')
+l, = axcontour.plot(RadLoc.loc[:,f0,Attempt],VertLoc.loc[:,f0,Attempt],color='Red',linewidth=3)
+axcontour.margins(x=0)
 
-f0 = JXA
+axprofile = fig.add_subplot(221) #plt.axes([0.25, 0.2, 0.4, 0.6], facecolor=axcolor)
+NeuDen.RadProf('NeuDen',LOG10=2,AX=axprofile,Markers=False,JXA=f0)
 
-l, = ax.plot(RadLoc.loc[:,f0,Attempt],VertLoc.loc[:,f0,Attempt],color='Red',linewidth=3)
-ax.margins(x=0)
+axslide = fig.add_subplot(223, facecolor=axcolor) #plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
+sslide = Slider(axslide, 'Poloidal Surface', 1, 98, valinit=f0, valfmt='%0.0f', valstep=1.0)
 
-axcolor = 'lightgoldenrodyellow'
-axfreq = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
-
-axprofile = plt.axes([0.25, 0.2, 0.4, 0.6], facecolor=axcolor)
-NeuDen.RadProf('NeuDen',LOG10=1,AX=axprofile,Markers=False,JXA=f0)
-
-sfreq = Slider(axfreq, 'Poloidal Surface', 24.0, 71.0, valinit=f0, valfmt='%0.0f', valstep=1.0)
 
 def update(val):
 
-    freq = sfreq.val
-    l.set_xdata(RadLoc.loc[:,freq,Attempt])
-    l.set_ydata(VertLoc.loc[:,freq,Attempt])
+    PolPos = sslide.val
+    l.set_xdata(RadLoc.loc[:,PolPos,Attempt])
+    l.set_ydata(VertLoc.loc[:,PolPos,Attempt])
     fig.canvas.draw_idle()
 
 
-sfreq.on_changed(update)
+sslide.on_changed(update)
 
 resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
 button = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975')
 
 
 def reset(event):
-    sfreq.reset()
+    sslide.reset()
     
 button.on_clicked(reset)
 
