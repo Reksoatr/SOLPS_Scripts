ea548af77b4d54f4daa51ef645af1ddb187ce00f
Richard Reksoatmodjo <rmreksoatmodjo@email.wm.edu>
2020-01-21 13:23:25 -0500

    Testing with EfoldSlider

--- SOLPS_Scripts/EfoldSliderGUI.py
+++ SOLPS_Scripts/EfoldSliderGUI.py
@@ -12,8 +12,8 @@ import matplotlib.gridspec as gridspec
 from matplotlib.widgets import Slider, Button, CheckButtons
 
 Shot = '12'
-Attempt = ['80', '81', '82', '83', '84', '85', '86', '87', '88', '89']
-PS=['.','.','.','.','.','.','.','.','.','.','-']
+Attempt = ['95','96','97','98','99']
+PS=['.','.','.','.','.','-']
 
 NeuDen = SOLPSPLOT(Shot,Attempt,'NeuDen',AVG=True,PlotScheme=PS)
 JXA = NeuDen.KW['JXA']
