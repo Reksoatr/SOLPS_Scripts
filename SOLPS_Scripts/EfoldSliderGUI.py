3b495f3b03a66a6a333f02be663c1b2687a28f30
Richard Reksoatmodjo <rmreksoatmodjo@email.wm.edu>
2020-03-25 17:50:59 -0400

    Added in Flux Expansion plot to EfoldSliderGui, and enabled left-right arrow control

--- SOLPS_Scripts/EfoldSliderGUI.py
+++ SOLPS_Scripts/EfoldSliderGUI.py
@@ -14,8 +14,8 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from matplotlib.widgets import Slider, Button, CheckButtons
 
-Shot = '12'
-Attempt = ['101'] #,'102','103','104','105']
+Shot = 'gas025'
+Attempt = ['46'] #,'102','103','104','105']
 PS=['.','.','.','.','.','-']
 
 NeuDen = SOLPSPLOT(Shot,Attempt,['Ne','NeuDen'],PlotScheme='x',EXP=False) #,AVG=True,PlotScheme=PS)
@@ -26,47 +26,54 @@ Rmax = 0.01
 Rmin = -0.01
 RadLoc = NeuDen.RadCoords['RadLoc']
 VertLoc = NeuDen.RadCoords['VertLoc']
-RR,Rexp,Rstr = NeuDen.GetRadCoords('rrsep',[0,0])
+RR = NeuDen.GetRadCoords('rrsep',[0,0])[0]
+Psin = NeuDen.GetRadCoords('psin',[0,0])[0]
 
 f0 = JXA
 p0 = [0,3.5e20,0.005,1e18,1e21]
 e0 = [1e15,100]
-x0 = []
-xi = []
+efold={}
+yparam={}
+eparam={}
+x0 = {}
+xi = {}
+fluxpsn = {}
 log = 2
 axcolor = 'lightgoldenrodyellow'
-FIT=0
-#depth=0.02
-#yfit = np.zeros(RR.shape[0],CoreBound[1]-CoreBound[0])
 
 fig, ax = plt.subplots()
 ax.set_frame_on(False)
 ax.set_axis_off()
-gs=gridspec.GridSpec(3,2,width_ratios=[5,3],height_ratios=[4,4,1],hspace=0.3)
+gs=gridspec.GridSpec(9,2,width_ratios=[5,3],height_ratios=[1,1,1,1,1,1,1,1,1],hspace=0.3)
 
-axcontour = fig.add_subplot(gs[:,1])
+axcontour = fig.add_subplot(gs[0:8,1])
 NeuDen.Contour('NeuDen',LOG10=1,AX=axcontour, Markers=False)
 axcontour.set_title('Neutral Density Contour')
 l, = axcontour.plot(RadLoc.loc[:,f0,Attempt[-1]],VertLoc.loc[:,f0,Attempt[-1]],color='Red',linewidth=3)
 axcontour.margins(x=0)
 
-neudenprofile = fig.add_subplot(gs[0]) #plt.axes([0.25, 0.2, 0.4, 0.6], facecolor=axcolor)
+neudenprofile = fig.add_subplot(gs[0:3,0]) #plt.axes([0.25, 0.2, 0.4, 0.6], facecolor=axcolor)
 NeuDen.RadProf('NeuDen',LOG10=log,AX=neudenprofile,Markers=False,RADC='rrsep',JXA=f0)  #,PlotScheme=['x'])
 XLim = neudenprofile.get_xlim()
 YLim = neudenprofile.get_ylim()
 
-neprofile = fig.add_subplot(gs[2])
+neprofile = fig.add_subplot(gs[3:6,0])
 NeuDen.RadProf('Ne',AX=neprofile,Markers=False,RADC='rrsep',JXA=f0)
 neprofile.set_xlim(XLim)
 NeYLim = neprofile.get_ylim()
 neprofile.set_title('')
 
-#ri = np.where(np.abs(RR.loc[:,f0,Attempt].values) > Rmin)[0][0]
-#rf = np.where(np.abs(RR.loc[:,f0,Attempt].values) < Rmax)[0][-1]
-#NDTrial = np.polyfit(RR.loc[ri:rf,f0,Attempt],np.log(NeuDen.PARAM['NeuDen'].loc[ri:rf,f0,Attempt]),1,full=True)
+fluxpsnprofile = fig.add_subplot(gs[6:9,0])
+fluxpsnprofile.plot(RR.loc[:,f0,Attempt[0]].values,Psin.loc[:,f0,Attempt[0]].values,'*')
+fluxpsnprofile.axvline(0.0,color='k')
+fluxpsnprofile.axhline(1.0,color='k')
+fluxpsnprofile.set_xlim(XLim)
+fluxpsnYLim = fluxpsnprofile.get_ylim()
+fluxpsnprofile.set_xlabel(r'$R-R_{sep}$ (m)')
+fluxpsnprofile.set_ylabel(r'$\psi_n$')
 
-axslide = fig.add_subplot(gs[4], facecolor=axcolor) #plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
-sslide = Slider(axslide, 'Poloidal\nSurface', CoreBound[0], CoreBound[1]-1, valinit=f0, valfmt='%0.0f', valstep=1.0)
+axslide = fig.add_subplot(gs[8,1], facecolor=axcolor) #plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
+sslide = Slider(axslide, 'Poloidal\nSurface', CoreBound[0], CoreBound[1], valinit=f0, valfmt='%0.0f', valstep=1.0)
 #axslide.set
 
 def update(val):
@@ -79,18 +86,42 @@ def update(val):
         log = 0
         
     PolPos = sslide.val
+    RR_SOLPS = RR.loc[:,PolPos,Attempt[0]].values
     l.set_xdata(RadLoc.loc[:,PolPos,Attempt[-1]])
     l.set_ydata(VertLoc.loc[:,PolPos,Attempt[-1]])
     neudenprofile.clear()
     neprofile.clear()
+    fluxpsnprofile.clear()
     NeuDen.RadProf('NeuDen',LOG10=log,AX=neudenprofile,Markers=False,RADC='rrsep',JXA=PolPos) #,PlotScheme=['x'])
     NeuDen.RadProf('Ne',AX=neprofile,Markers=False,RADC='rrsep',JXA=PolPos)
+    fluxpsnprofile.plot(RR.loc[:,PolPos,Attempt[0]].values,Psin.loc[:,PolPos,Attempt[0]].values,'*')
+    fluxpsnprofile.axvline(0.0,color='k')
+    fluxpsnprofile.axhline(1.0,color='k')
+    fluxpsnprofile.set_xlabel(r'$R-R_{sep}$ (m)')
+    fluxpsnprofile.set_ylabel(r'$\psi_n$')
+    
+    if PolPos in x0.keys(): 
+        neprofile.axvline(x0[PolPos])
+        neudenprofile.axvline(x0[PolPos])
+        fluxpsnprofile.axvline(x0[PolPos])
+        neprofile.axvline(xi[PolPos])
+        neudenprofile.axvline(xi[PolPos])
+        fluxpsnprofile.axvline(xi[PolPos])
+        
+    if PolPos in yparam.keys():
+        neprofile.plot(RR_SOLPS,TANH(RR_SOLPS,*yparam[PolPos]))
+        
+    if PolPos in eparam.keys():
+        neudenprofile.plot(RR_SOLPS, EXPFIT(RR_SOLPS,*eparam[PolPos]))
+        neudenprofile.text(RR_SOLPS[0],EXPFIT(RR_SOLPS[0],*eparam[PolPos]),'e-folding length={:.3f}mm'.format(efold[PolPos]),horizontalalignment='center',verticalalignment='top')
     
     if Fixed.get_status()[0] is True:
         neudenprofile.set_xlim(XLim)
         neudenprofile.set_ylim(YLim)
         neprofile.set_xlim(XLim)
         neprofile.set_ylim(NeYLim)
+        fluxpsnprofile.set_xlim(XLim)
+        fluxpsnprofile.set_ylim(fluxpsnYLim)
 
     neprofile.set_title('')
     fig.canvas.draw_idle()
@@ -98,15 +129,25 @@ def update(val):
 
 sslide.on_changed(update)
 
+def arrowclick(event):
+    if event.key == 'right':
+        sslide.set_val(sslide.val+1)
+    elif event.key == 'left':
+        sslide.set_val(sslide.val-1)
+    else:
+        pass        
+
+cid = fig.canvas.mpl_connect('key_press_event', arrowclick)
+
 logonax = plt.axes([0.125, 0.025, 0.075, 0.06], facecolor=axcolor)
-LogOn = CheckButtons(logonax, [r'Log$_{10}$ Scale'],[1])
+LogOn = CheckButtons(logonax, [r'Log$_{10}$ Scale'],[True])
 LogOn.on_clicked(update)
 
-fixedax = plt.axes([0.225, 0.025, 0.075, 0.06], facecolor=axcolor)
-Fixed = CheckButtons(fixedax, ['Fix Axes'],[1])
+fixedax = plt.axes([0.210, 0.025, 0.075, 0.06], facecolor=axcolor)
+Fixed = CheckButtons(fixedax, ['Fix Axes'],[True])
 Fixed.on_clicked(update)
 
-resetax = plt.axes([0.325, 0.025, 0.05, 0.06])
+resetax = plt.axes([0.295, 0.025, 0.05, 0.06])
 Reset = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975')
 
 def reset(event):
@@ -114,55 +155,57 @@ def reset(event):
     
 Reset.on_clicked(reset)
 
-tanhfitax = plt.axes([0.4, 0.025, 0.1, 0.06])
+tanhfitax = plt.axes([0.355, 0.025, 0.1, 0.06])
 TanhFit = Button(tanhfitax, 'Create Tanh Fit', color=axcolor, hovercolor='0.975')
 
 def tanhfit(event):
     PolPos=sslide.val
-    if FIT == 0:
+    RR_SOLPS = RR.loc[:,PolPos,Attempt[0]].values
+    
+    if PolPos not in yparam.keys():
         RR_SOLPS = RR.loc[:,PolPos,Attempt[0]].values
         Ne_SOLPS = NeuDen.PARAM['Ne'].loc[:,PolPos,Attempt[0]].values
         yfit=curve_fit(TANH, RR_SOLPS, Ne_SOLPS,p0)
-        yparam = yfit[0]
-        print('Poloidal Slice {:0.0f}: r0={:.3f}m, h={:.3e}m^-3, d={:.3f}m, b={:.3e}m^-3, m={:.3e}m^-4'.format(PolPos,*yparam))
-        neprofile.plot(RR_SOLPS,TANH(RR_SOLPS,*yparam))
-        x0.append(yparam[0]+yparam[2])
-        xi.append(yparam[0]-yparam[2])
-        neprofile.axvline(x0[-1])
-        neudenprofile.axvline(x0[-1])
-        neprofile.axvline(xi[-1])
-        neudenprofile.axvline(xi[-1])
-        
-        '''
-        for n, i in enumerate(range(CoreBound[0],CoreBound[1])):
-            yfit[:,n]=curve_fit(TANH, RR.loc[:,i,Attempt[0]].values, NeuDen.PARAM['Ne'].loc[:,i,Attempt[0]].values)
-        '''
+        yparam[PolPos] = yfit[0]
+        x0[PolPos] = yparam[PolPos][0]+yparam[PolPos][2]
+        xi[PolPos] = yparam[PolPos][0]-yparam[PolPos][2]
+
+    print('Poloidal Slice {:0.0f}: r0={:.3f}m, h={:.3e}m^-3, d={:.3f}m, b={:.3e}m^-3, m={:.3e}m^-4'.format(PolPos,*yparam[PolPos]))
+    neprofile.plot(RR_SOLPS,TANH(RR_SOLPS,*yparam[PolPos]))
+    neprofile.axvline(x0[PolPos])
+    neudenprofile.axvline(x0[PolPos])
+    fluxpsnprofile.axvline(x0[PolPos])
+    neprofile.axvline(xi[PolPos])
+    neudenprofile.axvline(xi[PolPos])
+    fluxpsnprofile.axvline(xi[PolPos])
         
 TanhFit.on_clicked(tanhfit)
 
-expfitax = plt.axes([0.525, 0.025, 0.1, 0.06])
+expfitax = plt.axes([0.465, 0.025, 0.1, 0.06])
 ExpFit = Button(expfitax, 'Create Exp. Fit', color=axcolor, hovercolor='0.975')
 
 def expfit(event):
     PolPos=sslide.val
-    if FIT == 0:
-        xr = x0[-1]
-        xri = xi[-1]
-        RR_SOLPS = RR.loc[:,PolPos,Attempt[0]].values
-        RR_i = np.where((RR_SOLPS>(xri)) & (RR_SOLPS<(xr)))[0]
-        RR_SOLPS=RR_SOLPS[RR_i]
+    xr = x0[PolPos]
+    xri = xi[PolPos]
+    RR_SOLPS = RR.loc[:,PolPos,Attempt[0]].values
+    RR_i = np.where((RR_SOLPS>(xri)) & (RR_SOLPS<(xr)))[0]
+    RR_SOLPS=RR_SOLPS[RR_i]
+        
+    if PolPos not in eparam.keys():
         NeuDen_SOLPS = NeuDen.PARAM['NeuDen'].loc[RR_i,PolPos,Attempt[0]].values
         exfit=curve_fit(EXPFIT,RR_SOLPS,NeuDen_SOLPS,e0)
-        eparam=exfit[0]
-        efold = 1000/eparam[1]
-        print(eparam)
-        print('Poloidal Slice {:0.0f}: e-folding length={:.3f}mm'.format(PolPos,efold))
-        print('Exponential fit from r-r_sep={:.3f}m to r-r_sep={:.3f}m'.format(RR_SOLPS[0],RR_SOLPS[-1]))
-        NeuDenFit = eparam[0] * np.exp(eparam[1]*RR_SOLPS)
-        neudenprofile.plot(RR_SOLPS, NeuDenFit)
-        neudenprofile.text(RR_SOLPS[0],NeuDenFit[0],'e-folding length={:.3f}mm'.format(efold),horizontalalignment='center',verticalalignment='top')
-        #neprofile.axvline(RR_SOLPS[0])
-        #neudenprofile.axvline(RR_SOLPS[0])
+        eparam[PolPos] = exfit[0]
+        efold[PolPos] = 1000/eparam[PolPos][1]
+        
+    print('Exponential fit from r-r_sep={:.3f}m to r-r_sep={:.3f}m'.format(RR_SOLPS[0],RR_SOLPS[-1]))
+    print('A0={:.3e}, lambda={:.3f}'.format(*eparam[PolPos]))
+    print('Poloidal Slice {:0.0f}: e-folding length={:.3f}mm'.format(PolPos,efold[PolPos]))
+    #NeuDenFit = eparam[0] * np.exp(eparam[1]*RR_SOLPS)
+    neudenprofile.plot(RR_SOLPS, EXPFIT(RR_SOLPS,*eparam[PolPos]))
+    neudenprofile.text(RR_SOLPS[0],EXPFIT(RR_SOLPS[0],*eparam[PolPos]),'e-folding length={:.3f}mm'.format(efold[PolPos]),horizontalalignment='center',verticalalignment='top')
+    #neprofile.axvline(RR_SOLPS[0])
+    #neudenprofile.axvline(RR_SOLPS[0])
         
 ExpFit.on_clicked(expfit)
 
