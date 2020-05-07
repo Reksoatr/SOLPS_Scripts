a54b071fbe562c0ce7e01912ca5d408e2298b680
Richard Reksoatmodjo <rmreksoatmodjo@email.wm.edu>
2020-05-06 20:25:02 -0700

    First stage of line-of-sight chord intersection algorithm implemented

--- SOLPS_Scripts/EireneContourPlot.py
+++ SOLPS_Scripts/EireneContourPlot.py
@@ -9,11 +9,17 @@ from TOOLS import SET_WDIR
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.tri as tri
+from matplotlib.path import Path
 
 Shot='d3d'
 Device='d3d'
 Attempt='70'
 MeshID='001'
+
+Slice=1
+P0=np.array([1.65,-0.65])
+P1=np.array([2,-1.1])
+Thresh=0.1
 
 BASEDRT, TOPDRT = SET_WDIR('solps-iter/runs/','')
 if 'd3d' in Shot:
@@ -55,12 +61,29 @@ for n in range(0,NT):
     EirAx.plot(Triangles[n*4:(n*4)+3,0],Triangles[n*4:(n*4)+3,1],'k-')
 '''
 
-IM=EirAx.tripcolor(TP,LogVal)
+#IM=EirAx.tripcolor(TP,LogVal)
 
 #EirAx.plot(tr,tz,'r.')
 
 EirAx.plot(VVFILE[:,0]/1000,VVFILE[:,1]/1000,'k-')
 
-EirAx.set_aspect('equal')
-EirAx.grid()
-plt.colorbar(IM,ax=EirAx)
+#plt.colorbar(IM,ax=EirAx)
+
+if Slice == 1:
+    PP=P1-P0
+    Theta=np.arctan(PP[1]/PP[0])
+    displace=Thresh*np.array([-np.sin(Theta),np.cos(Theta)])
+    P0A=P0+displace
+    P0B=P0-displace
+    P1A=P1+displace
+    P1B=P1-displace
+    EirAx.plot([P0[0],P1[0]],[P0[1],P1[1]],'b-')
+    EirAx.plot([P0A[0],P1A[0]],[P0A[1],P1A[1]],'g-')
+    EirAx.plot([P0B[0],P1B[0]],[P0B[1],P1B[1]],'r-')
+    Chord=Path([P0A,P1A,P1B,P0B])
+    Band=Chord.contains_points(np.vstack((tr,tz)).T)
+    IM=EirAx.tripcolor(TP,Band)
+    EirAx.set_aspect('equal')
+    EirAx.grid()
+    
+    
