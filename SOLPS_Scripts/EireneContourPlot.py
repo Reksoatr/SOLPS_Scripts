3f51d69372a6c45568a40b74095e334909762342
Richard Reksoatmodjo <rmreksoatmodjo@email.wm.edu>
2020-05-06 23:38:54 -0700

    Second stage of chord slicing implemented, now pulling values along chord and successfully plotting them

--- SOLPS_Scripts/EireneContourPlot.py
+++ SOLPS_Scripts/EireneContourPlot.py
@@ -17,9 +17,9 @@ Attempt='70'
 MeshID='001'
 
 Slice=1
-P0=np.array([1.65,-0.65])
-P1=np.array([2,-1.1])
-Thresh=0.1
+P0=np.array([1.4,-0.7])  #[1.65,-0.65])
+P1=np.array([1.0,-0.95])  #[2,-1.1])
+Thresh=0.05
 
 BASEDRT, TOPDRT = SET_WDIR('solps-iter/runs/','')
 if 'd3d' in Shot:
@@ -40,7 +40,7 @@ MolDen=np.loadtxt('{}/EirMol{}'.format(DRT,Attempt),usecols = (2))
 NeuEng=np.loadtxt('{}/AtomEnergy{}'.format(DRT,Attempt),usecols = (2))
 MolEng=np.loadtxt('{}/MolEnergy{}'.format(DRT,Attempt),usecols = (2))
 
-LogVal=np.ma.log10(NeuEng)
+LogVal=np.ma.log10(NeuDen)
 
 Nodes=np.fromfile('{}/{}.tria.{}.nodes'.format(BASEDRT,Device,MeshID),sep=' ')
 NN=int(Nodes[0])
@@ -61,13 +61,12 @@ for n in range(0,NT):
     EirAx.plot(Triangles[n*4:(n*4)+3,0],Triangles[n*4:(n*4)+3,1],'k-')
 '''
 
-#IM=EirAx.tripcolor(TP,LogVal)
-
-#EirAx.plot(tr,tz,'r.')
-
 EirAx.plot(VVFILE[:,0]/1000,VVFILE[:,1]/1000,'k-')
+IM=EirAx.tripcolor(TP,LogVal)
+EirAx.set_aspect('equal')
+EirAx.grid()
+plt.colorbar(IM,ax=EirAx)
 
-#plt.colorbar(IM,ax=EirAx)
 
 if Slice == 1:
     PP=P1-P0
@@ -80,10 +79,20 @@ if Slice == 1:
     EirAx.plot([P0[0],P1[0]],[P0[1],P1[1]],'b-')
     EirAx.plot([P0A[0],P1A[0]],[P0A[1],P1A[1]],'g-')
     EirAx.plot([P0B[0],P1B[0]],[P0B[1],P1B[1]],'r-')
-    Chord=Path([P0A,P1A,P1B,P0B])
-    Band=Chord.contains_points(np.vstack((tr,tz)).T)
-    IM=EirAx.tripcolor(TP,Band)
-    EirAx.set_aspect('equal')
-    EirAx.grid()
+    Bounds=Path([P0A,P1A,P1B,P0B])
+    Mask=Bounds.contains_points(np.vstack((tr,tz)).T)
+    
+    ChordX=np.ma.array(tr,mask=~Mask).compressed()
+    ChordY=np.ma.array(tz,mask=~Mask).compressed()
+    
+    EirAx.plot(ChordX,ChordY,'r.')
+    
+    Chord=np.sqrt((ChordX-P0[0])**2 + (ChordY-P0[1])**2)
+    Chord=Chord-Chord.min()
+    Band=np.ma.array(LogVal,mask=~Mask).compressed()
+    
+    ProfFig, ProfAx = plt.subplots()
+    ProfAx.plot(Chord,Band,'k.')
+
     
     
