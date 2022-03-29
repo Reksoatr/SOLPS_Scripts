ecae1edd5b36fd45a0d133614aa0ea410243c382
jamcrou <98612762+jamcrou@users.noreply.github.com>
2022-03-28 22:58:46 -0400

    Update steepest_descent.py

--- SOLPS_Scripts/steepest_descent.py
+++ SOLPS_Scripts/steepest_descent.py
@@ -63,118 +63,7 @@ def Loss(exper_shot, sol_run):
     loss = error(sol_run[1], sol_pts)
     return loss
 
-def Setup(func, params, run_step= 1, steps = 4, lib =22):
-    '''Sets up and runs many runs over the given parameter space, with steps
-    determining how many grid points in each direction.'''
-#    n = len(params)
-    space = []
-    for i in params:
-        ticks = (i[1] - i[0])/steps
-        meep = []
-        for j in range(steps+1):
-            meep.append(i[0] +j*ticks)
-        space.append(meep)
-    print(space)
-    x_1 = np.linspace(-.12, -.03, 5)
-    x_2 = np.linspace(-.02, .02, 10)
-    x = np.append(x_1, x_2)
-    for i_ct, i in enumerate(space[0]):
-        for j_ct, j in enumerate(space[1]):
-            for k_ct, k in enumerate(space[2]):
-#                enter = 'cd Attempt_{}{}{}'.format(i,j,k)
-                diff = func(x, a = i, b= j, e=k)
-                #os.system('nano b2.transport.inputfile')
-                Points0 = InputfileParser(file='b2.transport.inputfile.vi')
-                D_Points={'1' : np.array([x,diff])} #This is where the optimization method comes in
-                Full_Points={'1':D_Points['1'],'3':Points0['3'],'4':Points0['4']}
-                mkdir = 'cp -r base Attempt_{}{}{}_mk{}'.format(i_ct,j_ct,k_ct, run_step)            
-                os.system(mkdir)
-                WriteInputfile(file='/sciclone/scr20/gjcrouse/SOLPS/runs/OPT_TEST_{}/Attempt_{}{}{}_mk{}/b2.transport.inputfile'.format(lib,i_ct,j_ct,k_ct,run_step),points=Full_Points)
-                path_name = f'cd /sciclone/scr20/gjcrouse/SOLPS/runs/OPT_TEST_{lib}/Attempt_{i_ct}{j_ct}{k_ct}_mk{run_step}' #finish adding mk0
-                batch_writer(path_name, i_ct, j_ct, k_ct, run_step)
-                os.system('cp batch_use  /sciclone/scr20/gjcrouse/SOLPS/runs/OPT_TEST_{}/Attempt_{}{}{}_mk{}/batch'.format(lib,i_ct,j_ct,k_ct,run_step))
-                batch_run = 'qsub /sciclone/scr20/gjcrouse/SOLPS/runs/OPT_TEST_{}/Attempt_{}{}{}_mk{}/batch'.format(lib,i_ct,j_ct,k_ct,run_step)
-                os.system(batch_run)
-                os.system('cd ../')
-                    
-def Loss_Analysis(params, exper_shot, gfilen, run_step = 1, steps = 4):
-    '''Post Step Analysis using a comparison of a given experimental shot
-    to analyize loss and provided desired run for further optimization.'''
-#    n = len(params)
-    space = []
-    loss_pts = []
-    eq = equilibrium(gfile=gfilen)
-    for i in params:
-        ticks = (i[1] - i[0])/steps
-        meep = []
-        for j in range(steps+1):
-            meep.append(i[0] +j*ticks)
-        space.append(meep)                    
-    STARTING = .75
-    ENDING = 1.03
-    exp_data = np.loadtxt(exper_shot, usecols = (0,1))
-    exp_new = []
-    for R in exp_data:
-        if R[0] > STARTING:
-            if R[0] < ENDING:
-                exp_new.append(R)
-    exp_Data = np.array(exp_new)
-    exp_data = exp_Data.T
-    #print(exp_data)
-    tick = 0
-    for i_ct, i in enumerate(space[0]):
-        for j_ct, j in enumerate(space[1]):
-            for k_ct, k in enumerate(space[2]):
-                if run_step==1:
-                    enter = f'/sciclone/scr20/gjcrouse/SOLPS/runs/OPT_TEST_11/Attempt_{i_ct}{j_ct}{k_ct}'
-                else:
-                    enter = f'/sciclone/scr20/gjcrouse/SOLPS/runs/OPT_TEST_11/Attempt_{i_ct}{j_ct}{k_ct}_mk{run_step}' 
-                try:
-                    os.chdir(enter)
-                except:
-                    continue
-                os.system('pwd')
-                os.system('rm *.last10')
-                os.system('2d_profiles')
-                print('Attempt_{}{}{}'.format(i_ct,j_ct,k_ct))
-                try:
-                    Attempt = np.loadtxt('ne3da.last10')
-                except:
-                    continue
-                if len(Attempt) != 0:
-                    Attempt = Attempt.T
-                    R_sep = PsiN2R(eq, 1.0)
-                    for R in Attempt[0]:
-                        R = R2PsiN(eq,R+R_sep)
-                    l = Loss(exp_data, Attempt)
-                    loss_pts.append([l,i,j,k,tick])
-                tick += 1
-    b = np.amin(loss_pts, axis = 0)
-    print('initial guess is:', loss_pts[0])
-    print('Difference in loss is:', b[0]-loss_pts[0][0])
-    for i in loss_pts:
-        if b[0] == i[0]: 
-            b_new = i 
-            print('Minimum loss is at:')
-            print(i)
-    params_new = []
-    b_star = [b_new[1], b_new[2], b_new[3]]
-    with open('error.csv', mode = 'a') as f:
-        wri = csv.writer(f)
-        wri.writerow(b_new)
-    params_new.append(b_star)
-    temp = int(b_new[4])
-    loss_ptsb = np.delete(loss_pts, temp,0)
-    b1 = np.amin(loss_ptsb, axis = 0)
-    for i in loss_ptsb:
-        if b1[0] == i[0]: 
-            b_new = i 
-    b_star1 = [b_new[1], b_new[2], b_new[3]]
-    params_new.append(b_star1)
-    params_new = np.array(params_new)
-    params_news =  params_new.T
-    print(params_news)
-    return params_news
+
 #add last10 notes to look at different last10 file, check if last10 files need deleted
 #use mv command rm b2mn.prt  
 #ls -al
@@ -186,7 +75,7 @@ def Further_Analysis(params, exper_shot, gfilen, lib = 22, alpha =.3, run_step =
 #    n = len(params)
     eq = equilibrium(gfile=gfilen)
     space = []
-    learn = .1
+    learn = .2
     loss_pts = []
     for i in params:
         l =[]
@@ -233,7 +122,7 @@ def Further_Analysis(params, exper_shot, gfilen, lib = 22, alpha =.3, run_step =
             loss_pts.append([l,i[0],i[1], i[2], i[3], i_ct])
     b = np.amin(loss_pts, axis = 0)
     print('initial guess is:', loss_pts[0])
-    print('Difference in loss is:', b[0]-loss_pts[0][0])
+    print('Difference in loss is:', loss_pts[0][0]-b[0])
     if b[0] == loss_pts[0][0]:
         print('guess too far')
     elif b[0] == loss_pts[1][0]:
@@ -265,7 +154,7 @@ def Loss_Graph(csv):
 
 def Further_Steps(func, params, alpha = .3, run_step=2, lib = 22,Post_Analysis = True, exper_shot = None, gfilen = None):
     space = []
-    learn = .1
+    learn = .2
     for i in params:
         l =[]
         step_0 = learn*alpha*i+i
@@ -346,9 +235,9 @@ if __name__ == '__main__':
     blep =int(blep)
     data_analysis = input('Is this data analysis after a run? (y or n)')
     if blep != 1:
-        #losm = np.loadtxt('params.txt')
-        #guess_init = losm[0]
-        #loss_val = losm[1]
+        losm = np.loadtxt('params.txt')
+        guess_init = [losm[0], losm[1],losm[2],losm[3]]
+        loss_val = losm[4]
         h=1
     if data_analysis == 'y':
         guess_init, loss_val = Further_Analysis(guess_init, '/sciclone/scr20/gjcrouse/SOLPS/runs/OPT_TEST_03/yag.txt', 'g027205.00275_efitpp', run_step = blep,alpha=loss_val)
@@ -362,10 +251,8 @@ if __name__ == '__main__':
 '''
 data = [[1, 1.018],
         [2,	1.0182801375187802-0.029898925908849794],
-        [3, 0.8555],
-[3,	0.337],
-[4,	0.7605],
-[5,	0.713]]
+        [3, .931]
+        [4,]
 data = np.array(data).T
 plt.figure()
 plt.plot(data[0], data[1], '-')
@@ -385,4 +272,5 @@ axs.plot(test, test_y, color = 'b', label = 'Original')
 axs.plot(x, y, color = 'g', label = 'Training')
 #axs.plot(x, y_Lit, color = 'm', label = 'Literature')
 axs.legend()
+
 '''
