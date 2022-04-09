40e484558b793ac0470285e79364f8f22e7a19d4
jamcrou <98612762+jamcrou@users.noreply.github.com>
2022-04-09 16:22:01 -0400

    x

--- SOLPS_Scripts/steepest_trainer.py
+++ SOLPS_Scripts/steepest_trainer.py
@@ -113,7 +113,7 @@ def Further_Analysis(params, exper_shot, gfilen, lib = 5, alpha =.3, run_step = 
         Attempt = attempt.T
         l = Loss(exp_data, Attempt, plot=True, ice = run_step, lib = lib, run_step=run_step)
         print(l)
-        b = alpha-l
+        b = (alpha-l)*2
         if run_step == 1:
             b= l/3
 
