f08b5c28e0702e61d3464734d26b91c8ac5baf76
jamcrou <98612762+jamcrou@users.noreply.github.com>
2022-04-09 03:39:30 -0400

    Update File_for_longruns.py

--- SOLPS_Scripts/File_for_longruns.py
+++ SOLPS_Scripts/File_for_longruns.py
@@ -3,9 +3,9 @@ import os
 def batch_extended(lib, start, end):
     f = open('batch_use_extended', 'w')
     f.writelines(['#!/bin/tcsh','\n#PBS -l nodes=1:hima:ppn=1','\n#PBS -l walltime=24:00:00','\n#PBS -N LARGE','\n#PBS -j oe','\n','\nenv','\n',f'\ncd /sciclone/scr20/gjcrouse/SOLPS/runs/OPT_TEST_{lib}/'])
-    #f.writelines(['\npython3 -m steepest_descent_analysis <<Inputblock','\n1','\nn', '\nInputblock'])
-    #f.writelines([f'\ncd /sciclone/scr20/gjcrouse/SOLPS/runs/OPT_TEST_{lib}/Attempt_1\n','\nb2run b2mn > run.log'])
-    #f.writelines([f'\ncd /sciclone/scr20/gjcrouse/SOLPS/runs/OPT_TEST_{lib}'])
+    f.writelines(['\npython3 -m steepest_descent_analysis <<Inputblock','\n1','\nn', '\nInputblock'])
+    f.writelines([f'\ncd /sciclone/scr20/gjcrouse/SOLPS/runs/OPT_TEST_{lib}/Attempt_1\n','\nb2run b2mn > run.log'])
+    f.writelines([f'\ncd /sciclone/scr20/gjcrouse/SOLPS/runs/OPT_TEST_{lib}'])
     for i in range(start,end):
         f.writelines(['\npython3 -m steepest_descent_analysis <<Inputblock',f'\n{i}','\ny', '\nInputblock'])
         f.writelines(['\npython3 -m steepest_descent_analysis <<Inputblock',f'\n{i+1}','\nn', '\nInputblock'])
@@ -13,6 +13,6 @@ def batch_extended(lib, start, end):
         f.writelines([f'\ncd /sciclone/scr20/gjcrouse/SOLPS/runs/OPT_TEST_{lib}'])
     f.close()
 if __name__ == '__main__':
-    blip = 3
+    blip = 4
     batch_extended(blip, 2,18)
     os.system('qsub batch_use_extended')
