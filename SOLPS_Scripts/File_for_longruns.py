009d0e7089b33566be91266dfbeb4260d982032b
jamcrou <98612762+jamcrou@users.noreply.github.com>
2022-04-09 04:15:20 -0400

    Update File_for_longruns.py

--- SOLPS_Scripts/File_for_longruns.py
+++ SOLPS_Scripts/File_for_longruns.py
@@ -2,13 +2,13 @@ import os
 #need to fix the script later
 def batch_extended(lib, start, end):
     f = open('batch_use_extended', 'w')
-    f.writelines(['#!/bin/tcsh','\n#PBS -l nodes=1:hima:ppn=1','\n#PBS -l walltime=24:00:00','\n#PBS -N LARGE_T','\n#PBS -j oe','\n','\nenv','\n',f'\ncd /sciclone/scr20/gjcrouse/SOLPS/runs/OPT_TEST_{lib}/'])
-    f.writelines(['\npython3 -m steepest_trainer <<Inputblock','\n1','\nn', '\nInputblock'])
+    f.writelines(['#!/bin/tcsh','\n#PBS -l nodes=1:hima:ppn=1','\n#PBS -l walltime=24:00:00','\n#PBS -N LARGE_D','\n#PBS -j oe','\n','\nenv','\n',f'\ncd /sciclone/scr20/gjcrouse/SOLPS/runs/OPT_TEST_{lib}/'])
+    f.writelines(['\npython3 -m steepest_descent_analysis <<Inputblock','\n1','\nn', '\nInputblock'])
     f.writelines([f'\ncd /sciclone/scr20/gjcrouse/SOLPS/runs/OPT_TEST_{lib}/Attempt_1\n','\nb2run b2mn > run.log'])
     f.writelines([f'\ncd /sciclone/scr20/gjcrouse/SOLPS/runs/OPT_TEST_{lib}'])
     for i in range(start,end):
-        f.writelines(['\npython3 -m steepest_trainer <<Inputblock',f'\n{i}','\ny', '\nInputblock'])
-        f.writelines(['\npython3 -m steepest_trainer <<Inputblock',f'\n{i+1}','\nn', '\nInputblock'])
+        f.writelines(['\npython3 -m steepest_descent_analysis <<Inputblock',f'\n{i}','\ny', '\nInputblock'])
+        f.writelines(['\npython3 -m steepest_descent_analysis <<Inputblock',f'\n{i+1}','\nn', '\nInputblock'])
         f.writelines(['\n',f'cd /sciclone/scr20/gjcrouse/SOLPS/runs/OPT_TEST_{lib}/Attempt_{i+1}\n','\nb2run b2mn > run.log'])
         f.writelines([f'\ncd /sciclone/scr20/gjcrouse/SOLPS/runs/OPT_TEST_{lib}'])
     f.close()
