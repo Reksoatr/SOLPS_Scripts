407ef1da1334337b90a017f76765538f5ec46f5d
jamcrou <98612762+jamcrou@users.noreply.github.com>
2022-04-10 01:32:42 -0400

    b

--- SOLPS_Scripts/steepest_descent_analysis.py
+++ SOLPS_Scripts/steepest_descent_analysis.py
@@ -129,9 +129,11 @@ def Further_Analysis(params, exper_shot, gfilen, lib = 4, alpha =.3, run_step = 
         l = Loss(exp_data, Attempt, plot=True, ice = run_step, lib = lib, run_step=run_step)
         print(l)
         b = (alpha-l)*10
+        while np.abs(b) < .1:
+            b = b*10
         if run_step == 1:
             b= .4
-
+            
     print('Difference in loss is:', b)
     if b==0:
         params_news = params
