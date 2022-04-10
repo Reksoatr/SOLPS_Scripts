668e104e1daccce9956d433dbfd12b8c5da95163
jamcrou <98612762+jamcrou@users.noreply.github.com>
2022-04-10 13:08:57 -0400

    Update train_test2.py

--- SOLPS_Scripts/train_test2.py
+++ SOLPS_Scripts/train_test2.py
@@ -113,7 +113,7 @@ def Further_Analysis(params, exper_shot, gfilen, lib = 5, alpha =.3, run_step = 
         Attempt = attempt.T
         l = Loss(exp_data, Attempt, plot=True, ice = run_step, lib = lib, run_step=run_step)
         print(l)
-        b = (alpha-l)*40
+        b = (alpha-l)*15
         while np.abs(b) > 1:
             b = b/10
     print('Difference in loss is:', b)
